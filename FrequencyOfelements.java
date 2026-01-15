e9d2505b031bfe2d010e6a9748274243339367f9
sumanth8022 <gollasumanth9@gmail.com>
2026-01-15 12:43:31 +0530

    Message

--- FrequencyOfelements.java
+++ FrequencyOfelements.java
@@ -1,9 +1,30 @@
 package Com.cg;
 
+import java.util.Arrays;
+
 public class FrequencyOfelements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = {1,4,5,1,6,7,8};
+		int n = arr.length;
+		boolean visited[] = new boolean[n];
+		Arrays.fill(visited, false);
+		for(int i=0;i<n;i++) {
+			if(visited[i]==true) {
+				continue;
+			}
+			int count = 1;
+			for(int j=i+1;j<n;j++) {
+				if(arr[i]==arr[j]) {
+					visited[j] = true;
+					count++;
+				}
+			}
+			System.out.println(arr[i] + " "+count);
+		}
+		
+		
 		
 
 	}
---
+++ RepeatingELements.java
@@ -0,0 +1,11 @@
+package Com.cg;
+
+public class RepeatingELements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,5,6,7,1,5};
+
+	}
+
+}
